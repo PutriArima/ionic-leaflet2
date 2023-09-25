bb9befc8c4f6a13091297f6b65c88b09da21447d
PutriArima <putriarimasari@gmail.com>
2023-09-25 10:19:18 +0700

    base project ionic Leaflet

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  map! : L.Map;
+
   constructor() {}
 
+  ionViewDidEnter(){
+    this.map = L.map('mapId').setView([51.505, -0.09], 10)
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png',{
+      attribution: '&copy; <a href = "https://www.openstreetmap.org/copyright" >OpenStreetMap</a> contributors'
+    }).addTo(this.map); 
+
+    const markerIcon = L.icon({
+      iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+      iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+      iconSize: [25,41],
+      iconAnchor: [12,41],
+    });
+
+    const marker = L.marker([51.505, -0.09], {icon: markerIcon}).addTo(this.map)
+    .bindPopup('This Is My Info Window.')
+    .openPopup();
+  }
+ 
 }
